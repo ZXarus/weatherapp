d61790904166388657a669ba187aa82c2de9587b
adithya-ummadi <adithyaummadi999@gmail.com>
2025-05-04 10:09:24 +0530

    Updated background App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Weather from './components/Weather'
+import background from './assets/background.jpeg'
+
 const App = () => {
   return (
-    <div className='app'>
+    <div
+      className="app"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <h1 className="title">Team Shamrock</h1>
-      <Weather/>
+      <Weather />
     </div>
   )
 }
